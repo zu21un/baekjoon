71fafbbe75748334a8111e952f53f11ca8518d0e
zu21un <jwjoo03@gmail.com>
2022-01-13 00:00:27 +0900

    주유소_100점

--- 주유소.py
+++ 주유소.py
@@ -14,4 +14,12 @@ min_cost = cost[0]
 min_idx = 0
 for i in range(1, len(dist)):
     if cost[i] <= min_cost:
-        sum(dist[min_idx:i])
+        result += min_cost * sum(dist[min_idx:i])
+        rest_dist -= sum(dist[min_idx:i])
+        min_idx = i
+        min_cost = cost[min_idx]
+
+if rest_dist != 0:
+    result += min_cost * rest_dist
+
+print(result)
